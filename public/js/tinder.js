3b96eab0a05f0d71d73985d1d4f70ae115de398b
Victor <victor.chanet.dev@gmail.com>
2024-05-03 10:58:52 +0200

    Update tinder.js

--- public/js/tinder.js
+++ public/js/tinder.js
@@ -1,7 +1,6 @@
 function displayLikeAnimation() {
     const post = document.getElementsByClassName("post")[0];
-    post.style.transform = "translateY(100vh)";
-    post.style.transform = "translateX(60vw)";
+    post.style.transform = "translateY(100vh) translateX(60vw)";
 
     post.style.backgroundColor = "rgba(0,255,0,0.4)";
     post.style.boxShadow = "0 0px 15px rgba(0, 255, 0, 0.3)";
