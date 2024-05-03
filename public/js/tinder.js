41d390fd3765225c4db176c400cbbc066aaeb517
Victor <victor.chanet.dev@gmail.com>
2024-05-03 11:08:31 +0200

    Update tinder.js

--- public/js/tinder.js
+++ public/js/tinder.js
@@ -2,7 +2,7 @@ function displayLikeAnimation() {
     const post = document.getElementsByClassName("post")[0];
     post.style.transform = "translateY(100vh)";
 
-    post.style.animation = 'swipeRight 0.5s';
+    post.style.animation = 'swipeRight 0.7s';
 
     confetti({
         particleCount: 100,
