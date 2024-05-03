89147dd03188b883c10d33f8df37c2d400afb457
Victor <victor.chanet.dev@gmail.com>
2024-05-04 01:31:10 +0200

    Update post.js

--- deno_modules/post.js
+++ deno_modules/post.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/client/deno/edge.ts';
+import { PrismaClient } from '../generated/client/deno/edge.js';
 import { addXP, EXPERIENCE_TABLE } from "./rpg.js";
 
 const prisma = new PrismaClient();
