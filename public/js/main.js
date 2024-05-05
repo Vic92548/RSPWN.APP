3b37af264f81d88d80ab6f62f4ab8e2e2cfb443e
Victor <victor.chanet.dev@gmail.com>
2024-05-05 21:08:33 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -264,6 +264,8 @@ function displayPost(postId = undefined){
 
 
             history.pushState(null, null, "/post/" + data.id);
+
+            displayPost();
         }else{
             makeApiRequest("/feed", false).then(data => {
 
@@ -272,8 +274,7 @@ function displayPost(postId = undefined){
                 feed_posts = data.sort((a, b) => 0.5 - Math.random());
 
                 console.log(data);
-
-                displayPost();
+                
             }).catch(error => {
                 console.log(error);
             });
