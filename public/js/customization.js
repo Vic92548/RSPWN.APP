80fa7f447ae8986fceb26bb6bd4c42b0d3b31cdb
Victor <victor.chanet.dev@gmail.com>
2024-05-08 18:01:15 +0200

    c

--- public/js/customization.js
+++ public/js/customization.js
@@ -1,3 +1,11 @@
 function equipBackground(url) {
     document.body.style.backgroundImage = 'url(' + url + ')';
 }
+
+function openCustomizationMenu() {
+    document.getElementById("background_images").style.display = "flex";
+}
+
+function closeCustomizationMenu() {
+    document.getElementById("background_images").style.display = "none";
+}
