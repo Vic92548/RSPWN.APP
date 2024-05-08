b2e73afcd56a94356c7a3b764bddbbca70425c8d
Victor <victor.chanet.dev@gmail.com>
2024-05-08 18:51:35 +0200

    Create background_images.js

---
+++ public/js/background_images.js
@@ -0,0 +1,27 @@
+background_images = [
+    {
+        "id" : "ninja_1",
+        "title" : "ninja 1",
+        "unlock" : 0
+    },
+    {
+        "id" : "anime_city_night",
+        "title" : "Anime city at night",
+        "unlock" : 1
+    },
+    {
+        "id" : "fantasy_forest",
+        "title" : "Fantasy forest",
+        "unlock" : 1
+    },
+    {
+        "id" : "boss_fight_1",
+        "title" : "Boss fight 1",
+        "unlock" : 2
+    },
+    {
+        "id" : "opex_1",
+        "title" : "opex 1",
+        "unlock" : 2
+    },
+]
