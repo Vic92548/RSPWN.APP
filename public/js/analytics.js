0bb8d26675e94e45ad0b368dcea4b2a5fd887bce
Victor <victor.chanet.dev@gmail.com>
2024-05-10 14:29:19 +0200

    analytics

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -6,12 +6,19 @@ function openAnalytics() {
     makeApiRequest("/me/posts", true).then(data => {
         console.log(data);
 
+        old_posts_container.innerHTML = "";
         for (let i = 0; i < data.length; i++) {
             const post = data[i];
-            old_posts_container.innerHTML += '<li>\n' +
-                '                        <span>' + post.title + '</span>\n' +
-                '                        <div><span><i class="fa-solid fa-eye"></i> 22</span> <a href="/post/' + post.id + '">open</a></div>\n' +
-                '                    </li>'
+
+            const li = document.createElement("li");
+            const title = document.createElement("span");
+            title.textContent = post.title;
+
+            li.appendChild(title);
+            const stats = document.createElement("div");
+            stats.innerHTML = '<div><span><i class="fa-solid fa-eye"></i> 22</span> <a href="/post/' + post.id + '">open</a></div>';
+
+            li.appendChild(stats);
         }
     })
 
