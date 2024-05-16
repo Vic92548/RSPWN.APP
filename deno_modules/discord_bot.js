ff0325d07a3fe16cfedc2c207036a13562372f64
Victor <victor.chanet.dev@gmail.com>
2024-05-16 18:40:39 +0200

    Update discord_bot.js

--- deno_modules/discord_bot.js
+++ deno_modules/discord_bot.js
@@ -1,13 +1,17 @@
 import { load } from 'https://deno.land/std@0.212.0/dotenv/mod.ts'
-import { createBot } from 'npm:@discordeno/bot@19.0.0-next.d81b28a'
+import { createBot, Intents,Collection } from 'npm:@discordeno/bot@19.0.0-next.d81b28a'
 
 async function startDylan() {
     const bot = createBot({
         token: Deno.env.get("DISCORD_BOT_TOKEN"),
+        intents: Intents.Guilds | Intents.GuildMessages,
         events: {
             ready: ({ shardId }) => {
                 console.log(`Shard ${shardId} ready`);
                 listGuilds(bot);
+            },
+            messageCreate: function (bot, message) {
+                console.log('message arrived --> ' + message);
             },
         },
     })
@@ -16,6 +20,7 @@ async function startDylan() {
 }
 
 async function listGuilds(bot) {
+    console.log(bot);
     const guilds = bot.guilds;
     for (const [guildId, guild] of guilds) {
         console.log(`Guild ID: ${guildId}, Guild Name: ${guild.name}`);
