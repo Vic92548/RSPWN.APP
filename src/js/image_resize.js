12085fb578086c963b4cc31e625152543de1c924
Victor <victor.chanet.dev@gmail.com>
2024-05-19 23:55:16 +0200

    fix

--- src/js/image_resize.js
+++ src/js/image_resize.js
@@ -1,11 +1,13 @@
 function makeImageBig(img) {
     img.classList.remove("normal_image");
     img.classList.add("big_image");
+    window.analytics.track('image_zoom_out', { post: current_post });
 }
 
 function makeImageNormal(img) {
     img.classList.remove("big_image");
     img.classList.add("normal_image");
+    window.analytics.track('image_zoom_in', { post: current_post });
 }
 
 function switchImage(img) {
