a635d1e66d7b65685eaf886a85640bc68f0ffe7c
Victor <victor.chanet.dev@gmail.com>
2024-05-23 22:54:34 +0200

    reactions

--- src/js/main.js
+++ src/js/main.js
@@ -313,64 +313,6 @@ function formatViews(viewCount) {
     }
 }
 
-function incrementEmoji(emoji) {
-    const emoji_count = document.getElementById(emoji);
-
-    emoji_count.textContent = (parseInt(emoji_count.textContent) + 1).toString();
-}
-
-function resetEmoji(emoji) {
-    const emoji_count = document.getElementById(emoji);
-    emoji_count.textContent = "0";
-}
-
-function addReaction(emoji) {
-    if (!isUserLoggedIn()) {
-        openRegisterModal();  // Ensure the user is logged in before allowing a reaction
-        return;
-    }
-
-    window.analytics.track('Add reaction',
-        {
-            post: current_post,
-            emoji,
-        });
-
-    incrementEmoji(emoji);
-
-    const path = `/add-reaction?postId=${current_post_id}&emoji=${encodeURIComponent(emoji)}`;
-    makeApiRequest(path).then(data => {
-        console.log('Reaction added:', data);
-    }).catch(error => {
-        console.error('Error adding reaction:', error);
-        alert('Error adding reaction. Please try again.');
-    });
-}
-
-
-function displayReactions() {
-    resetEmoji('💩');
-    resetEmoji('👀');
-    resetEmoji('😂');
-    resetEmoji('❤️');
-    resetEmoji('💯');
-
-    const path = `/get-reactions?postId=${current_post_id}`;
-
-    console.log("Post id : " + current_post_id);
-
-    makeApiRequest(path, false).then(data => {
-        console.log('Reactions received:', data);
-
-        for (let i = 0; i < data.reactions.length; i++) {
-            incrementEmoji(data.reactions[i].emoji);
-        }
-    }).catch(error => {
-        console.error('Error retrieving reactions:', error);
-        alert('Error retrieving reactions. Please refresh the page.');
-    });
-}
-
 function openMenu() {
     document.getElementById("menu").style.display = 'flex';
 }
